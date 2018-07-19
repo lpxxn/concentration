bc3ab17a519a1dc8a4a6a8c1a73f4244d8191e62
lpxxn <mi_duo@live.com>
2018-07-19 22:53:22 +0800

    open a new window

---
+++ ios11learn/ios11learn/RegisterUserViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterUserViewController.swift
+//  ios11learn
+//
+//  Created by li on 2018/7/19.
+//  Copyright © 2018年 li. All rights reserved.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios11learn/ios11learn/SignViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SignViewController.swift
+//  ios11learn
+//
+//  Created by li on 2018/7/19.
+//  Copyright © 2018年 li. All rights reserved.
+//
+
+import UIKit
+
+class SignViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+        print("Sign in button tapped")
+    }
+    
+    @IBAction func registerNewAccountButtonTapped(_ sender: Any) {
+        print("Register account button tapped")
+        
+        let registerViewController = self.storyboard?.instantiateViewController(withIdentifier: "RegisterUserViewController") as! RegisterUserViewController
+        self.present(registerViewController, animated: true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
