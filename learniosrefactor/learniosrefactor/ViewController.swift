112d09ccaa0ff3f265c00e19d1a2ce11d0a3e3c6
lpxxn <mi_duo@live.com>
2018-07-26 22:38:34 +0800

    refact class

---
+++ learniosrefactor/learniosrefactor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  learniosrefactor
+//
+//  Created by li on 2018/7/26.
+//  Copyright © 2018年 li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
